1dd2b2403cbc5fd74a396cd3a11ff52ef1fdadca
Dhatchayini-p <dhatcha2811@gmail.com>
2021-02-24 12:12:16 -0800

    [24022021][Dhacha]-Data Types

---
+++ Intro/src/DataTypes.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+public class DataTypes {
+
+    byte b=1;
+    short s=2;
+    int a=3;
+    float f=100.0f;
+    long l=876876876870L;
+    double d=5464.36546543165;
+
+
+
+    public void  printDataTypes(){
+
+        System.out.println(" byte : "+b);
+        System.out.println(" short : "+s);
+        System.out.println(" integer : "+a);
+        System.out.println(" float : "+f);
+        System.out.println(" long : "+l);
+        System.out.println(" double : "+d);
+    }
+}
---
+++ Intro/src/MainClass.java
@@ -0,0 +1,10 @@
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Main class starting point ");
+
+        DataTypes dataTypes=new DataTypes();
+        dataTypes.printDataTypes();
+    }
+}
