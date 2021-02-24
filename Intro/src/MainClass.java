241b5d6d92d3b21a729be643d7068ecd4e04eef3
Dhatchayini-p <dhatcha2811@gmail.com>
2021-02-24 12:32:27 -0800

    [24022021][Dhacha]-Data Types

--- Intro/src/MainClass.java
+++ Intro/src/MainClass.java
@@ -4,7 +4,9 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Main class starting point ");
 
-        DataTypes dataTypes=new DataTypes();
-        dataTypes.printDataTypes();
+//        DataTypes dataTypes=new DataTypes();
+//        dataTypes.printDataTypes();
+
+
     }
 }
